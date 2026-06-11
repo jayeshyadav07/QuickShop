d79a4199a1d84146400575984dca9aa0fe6f05e3
jayeshyadav07 <jayeshyadav8080@gmail.com>
2026-06-11 20:05:32 +0530

    mongodb connection configuration

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 
+// database connection
+connectDB();
+
+// express app
 const app = express();
 app.use(cors({ origin: '*' }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send('Welcome to QuickShop API');
+	res.send('Server is running');
 });
 
 app.listen(PORT, () => {
