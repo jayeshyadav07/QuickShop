750af05dfe25a7fb81eff197551d849c3600c2c7
jayeshyadav07 <jayeshyadav8080@gmail.com>
2026-06-20 14:20:38 +0530

    fix: remvoe richColor from Toaster

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
 		<AuthProvider>
 			<Provider store={store}>
 				<App />
-				<Toaster richColors closeButton />
+				<Toaster closeButton />
 			</Provider>
 		</AuthProvider>
 	</StrictMode>
